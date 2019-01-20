4ee49d44e306bde2bbd916847737a2632ea3aad6
ManDude <mandude@users.noreply.github.com>
2019-01-20 06:01:40 +0000

    Midboss card

--- reosd/th12/stage05mbs.c
+++ reosd/th12/stage05mbs.c
@@ -8,7 +8,7 @@ sub MBoss()
     clearEnemies();
     resetBossTime();
     enemyCreateRel("Ecl_EtBreak_ni", 0.0f, 0.0f, 9999, 0, 0);
-    setInvincible(120);
+    setInvincible(60);
     anmSelect(3);
     anmScriptSelect(0, 0);
     setFlags(64);
@@ -27,6 +27,7 @@ sub MBoss()
     anmSelect(3);
     resetBossTime();
     setAttack(0, 710, 2400, "MBossCard1");
+    setTimeoutAttack(0, "MBossEscape");
     setBossFog(160.0f, 11497327);
     setCheckpoint(6);
 	wait(30);
@@ -58,7 +59,7 @@ MBoss1_204:
 
 sub MBossCard1()
 {
-    var A B C D;
+    var A;
     resetBossTime();
     ins_21();
     clearEnemies();
@@ -75,202 +76,182 @@ sub MBossCard1()
     setInvincible(120);
     moveByTime(120, 4, 0.0f, 144.0f);
     setMoveArea(0.0f, 128.0f, 280.0f, 64.0f);
-    setAttack(0, 0, 2400, "MBossDead");
+    setAttack(0, 0, 2800, "MBossDead");
     setTimeoutAttack(0, "MBossEscape");
 !EN
-    ins_437(57, 2400, 500000, "ÂÆùÂ°î„Äå„Ç∞„É¨„Ç§„ÉÜ„Çπ„Éà„Éà„É¨„Ç∏„É£„Éº„Äç");
-!H
-    ins_439(59, 2400, 500000, "ÂÆùÂ°î„Äå„Ç∞„É¨„Ç§„ÉÜ„Çπ„Éà„Éà„É¨„Ç∏„É£„Éº„Äç");
-!L
-    ins_422(60, 2400, 500000, "ÂÆùÂ°î„Äå„Ç∞„É¨„Ç§„ÉÜ„Çπ„Éà„Éà„É¨„Ç∏„É£„Éº„Äç");
+    ins_437(57, 2400, 500000, "äÔèpÅuÉ~ÉXÉfÉBÉåÉNÉVÉáÉìÅv");
+!HL
+    ins_439(59, 2400, 500000, "äÔèpÅuå∂òfÉ~ÉXÉfÉBÉåÉNÉVÉáÉìÅv");
 !*
     setCheckpoint(43);
-    setLifebar(0, 2000.0f, -32640);
-    wait(60);
-    ins_263(0, 119);
-    playSE(31);
-    wait(60);
-    ins_269(0);
-    setKillbox(60.0f, 60.0f);
-    goto MBossCard1_2500 @ 0;
-MBossCard1_932:
-    ins_269(0);
-!E
-    %LOCAL1F = 3.141593f;
-!N
-    %LOCAL1F = 3.534292f;
-!HL
-    %LOCAL1F = 3.403392f;
-!*
-    $LOCAL4 = 49;
-    playSE(58);
-    setIntValDiff($C, 6, 8, 12, 12);
-    goto MBossCard1_1604 @ 0;
-MBossCard1_1192:
-    ins_81(%A, %B, %LOCAL1F, 16.0f);
-    %B = (%B - _f(32));
-    enemyCreateRel("MBossCard1Laser", %A, %B, 100, 10, 0);
-!E
-    %LOCAL1F = (%LOCAL1F + 1.047198f);
-!N
-    %LOCAL1F = (%LOCAL1F + 0.785398f);
-!HL
-    %LOCAL1F = (%LOCAL1F + 0.523599f);
-!*
-    wait(3);
-MBossCard1_1604:
-    if $C-- goto MBossCard1_1192 @ 0;
-    wait(210);
-    moveRandom(60, 4, 0.5f);
-    wait(61);
-    ins_269(0);
-!E
-    %LOCAL1F = 0.0f;
-!N
-    %LOCAL1F = 0.0f;
-!HL
-    %LOCAL1F = 0.0f;
-!*
-    $LOCAL4 = 55;
-    playSE(58);
-    setIntValDiff($D, 6, 8, 12, 12);
-    goto MBossCard1_2388 @ 0;
-MBossCard1_1976:
-    ins_81(%A, %B, %LOCAL1F, 16.0f);
-    %B = (%B - _f(32));
-    enemyCreateRel("MBossCard1Laser", %A, %B, 100, 10, 0);
-!E
-    %LOCAL1F = (%LOCAL1F - 1.047198f);
-!N
-    %LOCAL1F = (%LOCAL1F - 0.785398f);
-!HL
-    %LOCAL1F = (%LOCAL1F - 0.523599f);
-!*
-    wait(3);
-MBossCard1_2388:
-    if $D-- goto MBossCard1_1976 @ 0;
-    wait(210);
-    moveRandom(60, 4, 0.5f);
-    wait(61);
-MBossCard1_2500:
-    if 1 goto MBossCard1_932 @ 0;
-    goto MBossCard1_2588 @ 0;
-MBossCard1_2568:
-    wait(1000);
-MBossCard1_2588:
-    if 1 goto MBossCard1_2568 @ 0;
+120:
+	$A = $RAND % 2;
+START:
+	if ($A != 0) goto AT1 @ 120;
+	call("MBossCard1_at1");
+	goto AT_END @ 120;
+AT1:
+	call("MBossCard1_at2");
+AT_END:
+	$A = 1 - $A;
+	goto START @ 120;
     return();
 }
 
-sub MBossCard1Laser()
+sub MBossCard1_at1()
 {
-    var A B;
-    anmSelect(2);
-    ins_259(0, $LOCAL4);
-    ins_277(0, %LOCAL1F);
-    %B = %LOCAL1F;
-    setFlags(797);
-    setKillbox(60.0f, 60.0f);
-    callSeperate("MBossCard1LaserHit");
-    wait(60);
-    ins_275(0, 1);
-    $LOCAL1 = 1;
-    playSE(19);
-    wait(60);
-    callSeperate("MBossCard1LaserAt", _ff %B);
-    ins_275(0, 2);
-    $LOCAL1 = 2;
-    wait(30);
-    delete();
-    delete();
-}
-
-sub MBossCard1LaserAt(A)
-{
-    var B C D;
-    unless ($LOCAL4 == 55) goto MBossCard1LaserAt_296 @ 0;
-    bulletCreate(0);
-    bulletSetType(0, 2);
-    bulletSetSprite(0, 24, 0);
-    bulletSetCount(0, 2, 1);
-    bulletSetAngle(0, 0.0f, 0.032724924f);
-    bulletSetSpeed(0, 0.4f, 1.0f);
-    goto MBossCard1LaserAt_452 @ 0;
-MBossCard1LaserAt_296:
-    bulletCreate(0);
-    bulletSetType(0, 2);
-    bulletSetSprite(0, 23, 0);
-    bulletSetCount(0, 2, 1);
-    bulletSetAngle(0, 0.0f, 0.032724924f);
-    bulletSetSpeed(0, 0.4f, 1.0f);
-MBossCard1LaserAt_452:
-    bulletSetComplexTransform(0, 0, 1, 2, 1, -999999, -999999.0f, -999999.0f);
-    bulletSetComplexTransform(0, 1, 0, 268435456, 1, -999999, -999999.0f, -999999.0f);
-    bulletSetComplexTransform(0, 2, 0, 4, 120, -999999, 0.008333334f, -999999.0f);
-    unless ($RANK == 3) goto MBossCard1LaserAt_724 @ 0;
-    bulletSetComplexTransform(0, 2, 0, 4, 120, -999999, 0.0125f, -999999.0f);
-MBossCard1LaserAt_724:
-    %B = _f(0);
-    toRadianInterval(%A);
-    goto MBossCard1LaserAt_1164 @ 0;
-MBossCard1LaserAt_808:
-    ins_81(%C, %D, %A, %B);
-!EN
-    %B = (%B + _f(16));
+	var A;
+    moveByTime(40, 4, -96.0f, 144.0f);
+	bulletCreate(0);
+	bulletSetType(0, 2);
+	bulletSetSprite(0, 8, 2);
+	bulletSetAngle(0, 0.0f, 0.0f);
+	bulletSetComplexTransform(0, 1, 0, 1, -999999, -999999, -999999.0f, -999999.0f);
+	bulletSetComplexTransform(0, 0, 0, 2, 1, -999999, -999999.0f, -999999.0f);
+!HL
+	bulletSetComplexTransform(0, 2, 0, 256, 1, 14, -999999.0f, -999999.0f);
+!E
+	bulletSetCount(0, 16, 1);
+	bulletSetSpeed(0, 3.0f, 1.2f);
+!N
+	bulletSetCount(0, 24, 3);
+	bulletSetSpeed(0, 3.0f, 1.2f);
 !H
-    %B = (%B + _f(16));
+	bulletSetCount(0, 15, 2);
+	bulletSetSpeed(0, 1.5f, 0.8f);
 !L
-    %B = (%B + _f(13));
+	bulletSetCount(0, 19, 2);
+	bulletSetSpeed(0, 1.5f, 0.8f);
 !*
-    bulletSetAngle(0, %RANDRAD, 0.0f);
-    bulletSetOffset(0, %C, %D);
-    bulletShoot(0);
-    wait(1);
-MBossCard1LaserAt_1164:
-    if 1 goto MBossCard1LaserAt_808 @ 0;
-    return();
+	callToSlot("MBossCard1_at1_et0", 0);
+30:
+	noop();
+60:
+	noop();
+	moveDirect(96.0f, 96.0f);
+	terminateSlot(0);
+80:
+	$A = 3;
+	bulletCreate(0);
+	bulletSetType(0, 0);
+	bulletSetSprite(0, 20, 3);
+	bulletSetCount(0, 11, 4);
+	bulletSetComplexTransform(0, 2, 0, 0, -999999, -999999, -999999.0f, -999999.0f);
+!E
+	bulletSetAngle(0, 0.0f, 0.20943951023931954923084289221863f);
+	bulletSetSpeed(0, 3.5f, 1.2f);
+!N
+	bulletSetAngle(0, 0.0f, 0.20943951023931954923084289221863f);
+	bulletSetSpeed(0, 4.5f, 1.2f);
+!H
+	bulletSetAngle(0, 0.0f, 0.31415926535897932384626433832795f);
+	bulletSetSpeed(0, 5.5f, 1.2f);
+!L
+	bulletSetAngle(0, 0.0f, 0.31415926535897932384626433832795f);
+	bulletSetSpeed(0, 6.5f, 1.2f);
+!*
+	goto END @ 98;
+START:
+	bulletShoot(0);
+98:
+END:
+	if ($A--) goto START @ 80;
+158:
+	noop();
+218:
+	noop();
+	moveDirect(0.0f, 144.0f);
+	wait(30);
+	return();
 }
 
-sub MBossCard1LaserHit()
+sub MBossCard1_at1_et0()
 {
-    var A B;
-    ins_531(1);
-    %LOCAL1F = _f(0);
-    %LOCAL2F = _f(0);
-    goto MBossCard1LaserHit_284 @ 0;
-MBossCard1LaserHit_160:
-    unless ($LOCAL1 == 1) goto MBossCard1LaserHit_264 @ 0;
-    goto MBossCard1LaserHit_328 @ 0;
-MBossCard1LaserHit_264:
-    wait(1);
-MBossCard1LaserHit_284:
-    if 1 goto MBossCard1LaserHit_160 @ 0;
-MBossCard1LaserHit_328:
-    $A = 30;
-    goto MBossCard1LaserHit_564 @ 0;
-MBossCard1LaserHit_392:
-    %LOCAL1F = (%LOCAL1F + 0.666667f);
-    %LOCAL2F = (%LOCAL2F + 17.066668f);
-    wait(1);
-MBossCard1LaserHit_564:
-    if $A-- goto MBossCard1LaserHit_392 @ 0;
-    goto MBossCard1LaserHit_756 @ 0;
-MBossCard1LaserHit_632:
-    unless ($LOCAL1 == 2) goto MBossCard1LaserHit_736 @ 0;
-    goto MBossCard1LaserHit_800 @ 0;
-MBossCard1LaserHit_736:
-    wait(1);
-MBossCard1LaserHit_756:
-    if 1 goto MBossCard1LaserHit_632 @ 0;
-MBossCard1LaserHit_800:
-    $B = 10;
-    goto MBossCard1LaserHit_960 @ 0;
-MBossCard1LaserHit_864:
-    %LOCAL1F = (%LOCAL1F - 2.0f);
-    wait(1);
-MBossCard1LaserHit_960:
-    if $B-- goto MBossCard1LaserHit_864 @ 0;
-    return();
+	var;
+	noop();
+START:
+	bulletShoot(0);
+6:
+	goto START @ 0;
+	return();
+}
+
+sub MBossCard1_at2()
+{
+	var A;
+    moveByTime(40, 4, 96.0f, 144.0f);
+	bulletCreate(0);
+	bulletSetType(0, 2);
+	bulletSetSprite(0, 8, 2);
+	bulletSetAngle(0, 0.0f, 0.0f);
+	bulletSetComplexTransform(0, 1, 0, 1, -999999, -999999, -999999.0f, -999999.0f);
+	bulletSetComplexTransform(0, 0, 0, 2, 1, -999999, -999999.0f, -999999.0f);
+!HL
+	bulletSetComplexTransform(0, 2, 0, 256, 1, 14, -999999.0f, -999999.0f);
+!E
+	bulletSetCount(0, 16, 1);
+	bulletSetSpeed(0, 3.0f, 1.2f);
+!N
+	bulletSetCount(0, 24, 3);
+	bulletSetSpeed(0, 3.0f, 1.2f);
+!H
+	bulletSetCount(0, 12, 2);
+	bulletSetSpeed(0, 1.5f, 0.8f);
+!L
+	bulletSetCount(0, 16, 2);
+	bulletSetSpeed(0, 1.5f, 0.8f);
+!*
+	callToSlot("MBossCard1_at2_et0", 1);
+30:
+	noop();
+60:
+	noop();
+	moveDirect(-96.0f, 96.0f);
+	terminateSlot(1);
+80:
+	$A = 3;
+	bulletCreate(0);
+	bulletSetType(0, 0);
+	bulletSetSprite(0, 20, 3);
+	bulletSetCount(0, 11, 4);
+	bulletSetComplexTransform(0, 2, 0, 0, -999999, -999999, -999999.0f, -999999.0f);
+!E
+	bulletSetAngle(0, 0.0f, 0.20943951023931954923084289221863f);
+	bulletSetSpeed(0, 3.5f, 1.2f);
+!N
+	bulletSetAngle(0, 0.0f, 0.20943951023931954923084289221863f);
+	bulletSetSpeed(0, 4.5f, 1.2f);
+!H
+	bulletSetAngle(0, 0.0f, 0.31415926535897932384626433832795f);
+	bulletSetSpeed(0, 5.5f, 1.2f);
+!L
+	bulletSetAngle(0, 0.0f, 0.31415926535897932384626433832795f);
+	bulletSetSpeed(0, 6.5f, 1.2f);
+!*
+	goto END @ 98;
+START:
+	bulletShoot(0);
+98:
+END:
+	if ($A--) goto START @ 80;
+158:
+	noop();
+218:
+	noop();
+	moveDirect(0.0f, 144.0f);
+	wait(30);
+	return();
+}
+
+sub MBossCard1_at2_et0()
+{
+	var;
+	noop();
+START:
+	bulletShoot(0);
+6:
+	goto START @ 0;
+	return();
 }
 
 sub MBossDead()
@@ -281,8 +262,6 @@ sub MBossDead()
     unsetMoveArea();
     setAttack(0, -1, 0, "");
     setAttack(1, -1, 0, "");
-    moveByTime(0, 0, 0.0f, 0.0f);
-    enemySetTrajectory(_f((%RANDRAD / _f(4)) + -1.5707964f), 1.0f);
     enemyChangeMovement(60, 4, -999999.0f, 0.0f);
     playSE(28);
     unless ($TIMEOUT == 0) goto MBossDead_484 @ 0;
@@ -295,30 +274,18 @@ MBossDead_504:
     anmScriptSelect(0, 0);
     setLife(100000);
     setBossMode(-1);
-    moveByTime(60, 4, _f(%FINAL_X + _f(64)), -64.0f);
-    setBossMode(-1);
     clearItems();
-    setDeathItem(4);
-    setExtraItem(13, 1);
-    setExtraItem(14, 1);
+    if ($POWER == 400) goto MBossDead_864 @ 0;
+    setDeathItem(1);
+    setExtraItem(1, 60);
+    goto MBossDead_908 @ 0;
+MBossDead_864:
+    setDeathItem(2);
+    setExtraItem(2, 60);
+MBossDead_908:
     setItemArea(48.0f, 48.0f);
     dropItems();
-    wait(1);
-    $B = 30;
-    goto MBossDead_1100 @ 0;
-MBossDead_900:
-    clearItems();
-    setExtraItem(1, 3);
-    setItemArea(48.0f, 48.0f);
-    dropItems();
-    wait(1);
-    clearItems();
-    setExtraItem(2, 3);
-    setItemArea(48.0f, 48.0f);
-    dropItems();
-    wait(1);
-MBossDead_1100:
-    if $B-- goto MBossDead_900 @ 0;
+	call("MBossEscape");
     delete();
     delete();
 }
@@ -341,8 +308,9 @@ MBossEscape_284:
     setLife(100000);
     setBossMode(-1);
     setFlags(16);
-    moveByTime(60, 4, 0.0f, -32.0f);
-60:
+130:
+    moveByTime(60, 4, 0.0f, -64.0f);
+190:
     setCheckpoint(0);
     delete();
     delete();
@@ -404,4 +372,3 @@ MBoss_at1_1060:
     if $count-- goto MBoss_at1_812 @ 0;
     return();
 }
-
