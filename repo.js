f264cce3401a980be7c5edc961f0f1d62d553d63
ManDude <mandude@users.noreply.github.com>
2019-01-20 03:10:25 +0000

    Update repo.js

---
+++ repo.js
@@ -0,0 +1,9 @@
+{
+	"contact": "videomendude@gmail.com",
+	"id": "dass7",
+	"patches": {},
+	"servers": [
+		"https://raw.githubusercontent.com/ManDude/touhou-crap/master/"
+	],
+	"title": "DassRepo"
+}
