86d9165a02c3bb8d838fd9bdca5d59e5ddb088e0
ManDude <mandude@users.noreply.github.com>
2019-01-22 16:00:07 +0000

    Make warning message slightly less intimidating

--- reosd/patch.js
+++ reosd/patch.js
@@ -2,5 +2,5 @@
 	"dependencies": [
 		"nmlgc/base_tsa"
 	],
-	"title": "EoSD patch for UFO (incomplete)"
+	"title": "EoSD patch for UFO (WARNING: EXTREMELY INCOMPLETE EARLY ACCESS! *WILL* CORRUPT REGULAR GAME SCOREFILES!)"
 }
