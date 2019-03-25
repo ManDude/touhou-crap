1685f68ef7c877b0e5390d98f7786638ba3ce0d6
ManDude <mandude@users.noreply.github.com>
2019-03-25 16:26:24 +0000

    noshake: Add "noshake" patch.
    DDC-only: Makes the game ignore eclins_517 completely.

---
+++ noshake/patch.js
@@ -0,0 +1,6 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"title": "Replay-compatible screen shake removal. DDC-only for now."
+}
---
+++ noshake/th14.v1.00b.js
@@ -0,0 +1,9 @@
+{
+	"binhacks": {
+		"ecl_517_skip": {
+			"title": "Skip over ECL screen shake (ins_517).",
+			"addr": "Rx287e7",
+			"code": "eb 2b"
+		}
+	}
+}
