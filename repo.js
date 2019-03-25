e4f9ae2bd56ca97cd176a0a2c4a75178ca09cdb7
ManDude <mandude@users.noreply.github.com>
2019-03-25 16:27:03 +0000

    Remove disabled nmlgc repos.

--- repo.js
+++ repo.js
@@ -3,8 +3,6 @@
 	"id": "dass7",
 	"patches": {},
 	"servers": [
-		"https://thcrap.nmlgc.net/repos/dass7/",
-		"http://thcrap.nmlgc.net/repos/dass7/",
 		"https://mirrors.thpatch.net/dass7/",
 		"http://mirrors.thpatch.net/dass7/"
 	],
