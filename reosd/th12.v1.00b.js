69708d62cdaff77646ac8ae2377af2c1121022c9
ManDude <mandude@users.noreply.github.com>
2019-10-27 22:34:21 +0000

    reosd: replace replay folder (it is now called reosdreplay instead)

--- reosd/th12.v1.00b.js
+++ reosd/th12.v1.00b.js
@@ -4,6 +4,46 @@
 			"title": "Change the name of score.dat to scoreosd12.dat",
 			"addr": "Rxa14b0",
 			"code": "73 63 6F 72 65 6F 73 64 31 32 2E 64 61 74"
+		},
+		"replay_folder_name_change_1": {
+			"title": "Change the name of the replay folder to reosdreplay - #1 fill some static data",
+			"addr": "Rx97ff0",
+			"code": "72 65 6F 73 64 72 65 70 6C 61 79 2F 25 73"
+		},
+		"replay_folder_name_change_2": {
+			"title": "Change the name of the replay folder to reosdreplay - #2 replace 1st mention of the folder",
+			"addr": "Rx3bcf1",
+			"code": "68 f07f4900"
+		},
+		"replay_folder_name_change_3": {
+			"title": "Change the name of the replay folder to reosdreplay - #3 replace 2nd mention of the folder",
+			"addr": "Rx3c399",
+			"code": "68 f07f4900"
+		},
+		"replay_folder_name_change_4": {
+			"title": "Change the name of the replay folder to reosdreplay - #4 fill some static data again",
+			"addr": "Rx97fe0",
+			"code": "72 65 6F 73 64 72 65 70 6C 61 79"
+		},
+		"replay_folder_name_change_5": {
+			"title": "Change the name of the replay folder to reosdreplay - #5 replace 3rd mention of the folder",
+			"addr": "Rx3bce2",
+			"code": "68 e07f4900"
+		},
+		"replay_folder_name_change_6": {
+			"title": "Change the name of the replay folder to reosdreplay - #6 replace 4th mention of the folder",
+			"addr": "Rx465c7",
+			"code": "68 e07f4900"
+		},
+		"replay_folder_name_change_7": {
+			"title": "Change the name of the replay folder to reosdreplay - #7 replace 5th mention of the folder",
+			"addr": "Rx465d4",
+			"code": "68 e07f4900"
+		},
+		"replay_folder_name_change_8": {
+			"title": "Change the name of the replay folder to reosdreplay - #8 replace 6th mention of the folder",
+			"addr": "Rx4661c",
+			"code": "68 e07f4900"
 		}
 	}
 }
