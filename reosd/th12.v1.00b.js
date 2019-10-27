c3dfe0936dbf21af8319b66fee67a355d14b24ee
ManDude <mandude@users.noreply.github.com>
2019-10-27 22:09:52 +0000

    Create th12.v1.00b.js

---
+++ reosd/th12.v1.00b.js
@@ -0,0 +1,9 @@
+{
+	"binhacks": {
+		"score_dat_name_change": {
+			"title": "Change the name of score.dat to scoreosd12.dat",
+			"addr": "Rxa14b0",
+			"code": "73 63 6F 72 65 6F 73 64 31 32 2E 64 61 74"
+		}
+	}
+}
